76c1807a3a64cb5f2f28fb9ca8db1d270d92d361
homeworkmon <monica.emma.mason@gmail.com>
2022-04-18 12:17:28 -0400

    error notification on signup page

--- frontend/src/pages/createAccount.js
+++ frontend/src/pages/createAccount.js
@@ -13,23 +13,24 @@ const initialFieldValues = {
   username: '',
   password: ''
 }
-  
+
 const CreateAccount = ({ pageStyle }) => {
   const navigate = useNavigate()
+  const [severity, setSeverity] = useState('success')
   const [message, setMessage] = useState('')
   const [display, setDisplay] = useState(false)
 
-  const [ signUp ] = useMutation(CREATE_ACCOUNT , {
+  const [ signUp ] = useMutation(CREATE_ACCOUNT)
+  /* , {
     onError: (error) => {
-      setMessage(error.graphQLErrors[0].message)
+      setMessage(error.message)
+      setSeverity('error')
       setDisplay(true)
-      setTimeout(() => setDisplay(false), 1500)
-      setTimeout(() => setMessage(''), 1500)
-    } 
-  })
+    }
+  }) */
 
   const validate = (fieldValues = values) => {
-    let temp = {...errors}
+    let temp = { ...errors }
     if('username' in fieldValues) {
       temp.username = fieldValues.username.length<3 ? 'Please make username longer than 3 characters': ''
     }
@@ -38,12 +39,12 @@ const CreateAccount = ({ pageStyle }) => {
     }
     setErrors({ ...temp })
 
-    if(fieldValues == values)
-      return Object.values(temp).every(x => x == '')
+    if(fieldValues === values)
+      return Object.values(temp).every(x => x === '')
   }
 
   const {
-    values, 
+    values,
     setValues,
     errors,
     setErrors,
@@ -52,36 +53,47 @@ const CreateAccount = ({ pageStyle }) => {
 
   const handleSubmit = async (e) => {
     e.preventDefault()
-    await signUp({ variables: { ...values }})
-    setValues({})
-    setDisplay(true),
-    setMessage('Account created!')
+    console.log(values)
+    try {
+      await signUp({ variables: { ...values } })
+      setMessage('Account created!')
+      setSeverity('success')
+      setTimeout(
+        () => navigate('/'),
+        3000)
+    } catch(error) {
+      setMessage(error.message)
+      setSeverity('error')
+    }
+    setValues(initialFieldValues)
+    setDisplay(true)
     setTimeout(
-      () => navigate('/'),
-      3000)
+      () => setDisplay(false),
+      3000
+    )
   }
 
   return (
-    <div style={{...pageStyle, alignItems: 'center'}}>
-      <Typography variant='h4' sx={{justifySelf: 'flex-start', mt: 3}}>Create Account</Typography>
-      <Form 
-        onSubmit={handleSubmit} 
+    <div style={{ ...pageStyle, alignItems: 'center' }}>
+      <Typography variant='h4' sx={{ justifySelf: 'flex-start', mt: 3 }}>Create Account</Typography>
+      <Form
+        onSubmit={handleSubmit}
         style={{
           marginTop: '36px',
           width: '80%',
           display: 'flex',
           justifyContent: 'center',
           alignItems: 'center',
-          flexDirection: 'column'}}>
-        <Box sx={{width: {xs: '100%', sm: '50%', lg: '25%'}}}>
-          <Notification message={message} display={display} severity='success' sx={{m: 2}}/>
+          flexDirection: 'column' }}>
+        <Box sx={{ width: { xs: '100%', sm: '50%', lg: '25%' } }}>
+          <Notification message={message} display={display} severity={severity} sx={{ m: 2 }}/>
           <Stack spacing={3} align="center" sx={{
             '& .MuiFormControl-root': {
               width: '90%'
             }
           }}>
             <Input
-              name='username' 
+              name='username'
               label='Username'
               value={values.username}
               onChange={handleInputChange}
@@ -89,7 +101,7 @@ const CreateAccount = ({ pageStyle }) => {
             />
             <Input
               name='password'
-              type='password' 
+              type='password'
               label='Password'
               value={values.password}
               onChange={handleInputChange}
